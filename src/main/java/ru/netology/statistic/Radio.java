7cc03598d56803479f4c4d88c422265ca1efe7bb
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-05-28 17:52:05 +0800

    preliminarily

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -1,4 +1,33 @@
 package ru.netology.statistic;
 
 public class Radio {
+    public int currentStation;
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int nextStation;
+    public void setNextStation(int nextStation){
+        if (currentStation<9){
+            currentStation = currentStation +1;
+
+        }
+        this.nextStation = nextStation;
+    }
+    public int getNextStation(){
+        return nextStation;
+    }
+
+
 }
+
--- src/test/java/ru/netology/statistic/RadioTest.java
+++ src/test/java/ru/netology/statistic/RadioTest.java
@@ -1,4 +1,27 @@
 package ru.netology.statistic;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RadioTest {
+    @Test
+    void currentStation() {
+        Radio cond = new Radio();
+        cond.setCurrentStation(50);
+        int actual = cond.getCurrentStation();
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextStation() {
+        Radio cond = new Radio();
+        cond.setNextStation(2);
+        int actual = cond.getNextStation();
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
 }
+
